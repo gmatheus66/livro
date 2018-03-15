26e6b32c58f157a3b128b3e7b9875afb60ff7282
gmatheus66 <gmatheus66@hotmail.com>
2018-03-15 18:38:59 -0300

    novas atividades do livro(cap 3,4,5)

---
+++ cap5/CadastroDeLivros/src/cadastrodelivros/Autor.java
@@ -0,0 +1,18 @@
+package cadastrodelivros;
+
+
+public class Autor {
+    String nome;
+    String email;
+    String cpf;
+    
+    
+    
+    void mostrarDetalhes(){
+        System.out.println("Mostrando detalhes do autor ");
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("CPF: " + cpf);
+    }
+    
+}
